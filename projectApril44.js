95b5c31265c13e635502a6708f3e6e24a5beef5c
youyou-sj <126841348+youyou-sj@users.noreply.github.com>
2023-05-04 16:47:14 +0800

    MayAssignmentupdate

---
+++ projectApril44.js
@@ -0,0 +1,42 @@
+
+function check1(){
+  alert("Q1:加塞是客家话里的“吃完了”，同样的发音在福建话是吃大便的意思:D")}
+
+//礼物//
+function song1(){
+  var x = document.getElementById("songSILKROADyt");
+}
+function song2(){
+  var y = document.getElementById("songSILKROADb");}
+
+
+
+//Quiz选项//
+function answer1(){
+  alert("错了！小声告诉你‘加塞’是客家话里的“吃完了”，同样的发音在福建话是吃大便的意思:D")}
+
+function answer2(){
+  alert("答对了！！厉害！")}
+
+function answer3(){
+  alert("错了！再答多一次吧~")}
+
+function answer23(){
+  alert("错！嘿嘿！是不是很像，真的是是一个有趣的误区噢！")}
+
+function answer31(){
+  alert("错了！Aurora有这层意思在噢！再尝试看看吧！")}
+
+function answer34(){
+  alert ("答对了！Aurora有极光、晨曦的意思噢！例如北极光的英文是Aurora borealis. Aurora也是古罗马神话中的黎明女神！")}
+
+function answer41(){alert("海湾是一片三面环陆的海洋。可根据面积的大小而分为Gulf和Bay，如铜锣湾（Causeway Bay）、波斯湾（Persian Gulf）。")}
+function answer42(){
+  alert("丝路是丝绸之路,Silk Road，但其实我把这个选项放在这里是因为刚好在听梁静茹的《丝路》，真的很很很喜欢这首歌，推荐给打开这个对话框的你")}
+function answer43(){alert("盆地的英文是Basin噢！不过Basin同时也有水盆、容器等意思。")}
+function answer44(){alert("!!!!!!答对了!Oasis是绿洲的英文单词，想到这个题目是因为想到徐佳莹的《绿洲》，推荐给你！（不是那首失落沙洲，就是《绿洲》！）")}
+
+function answer54(){
+  alert("答对了！好厉害！不知道是从小学习马来文的关系吗，我就莫名觉得这个K/Khamis很有星期四的感觉和氛围，不知道你这样觉得吗哈哈哈")}
+
+function answer61(){alert("答对啦！你真的好叻（读作：lek！）噢！")}
